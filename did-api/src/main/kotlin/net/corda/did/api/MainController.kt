86774928a2e34b2eefa4378f1f553a68fcd5156b
nitesh_solanki <nitesh_solanki@persistent.com>
2019-05-17 14:07:24 +0530

    Add spring-boot gradle configuration for did-webserver api

---
+++ did-api/src/main/kotlin/net/corda/did/api/MainController.kt
@@ -0,0 +1,55 @@
+package net.corda.did.api
+
+
+import net.corda.core.identity.CordaX500Name
+import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
+import org.springframework.http.MediaType.*
+import org.springframework.web.bind.annotation.*
+
+val SERVICE_NAMES = listOf("Notary", "Network Map Service")
+
+/**
+ *  A Spring Boot Server API controller for interacting with the node via RPC.
+ */
+
+@RestController
+@RequestMapping("/api/example/")
+class MainController(rpc: NodeRPCConnection) {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(RestController::class.java)
+    }
+
+    private val myLegalName = rpc.proxy.nodeInfo().legalIdentities.first().name
+    private val proxy = rpc.proxy
+
+    /**
+     * Returns the node's name.
+     */
+    @GetMapping(value = [ "me" ], produces = [ APPLICATION_JSON_VALUE ])
+    fun whoami() = mapOf("me" to myLegalName)
+
+    /**
+     * Returns all parties registered with the network map service. These names can be used to look up identities using
+     * the identity service.
+     */
+    @GetMapping(value = [ "peers" ], produces = [ APPLICATION_JSON_VALUE ])
+    fun getPeers(): Map<String, List<CordaX500Name>> {
+        val nodeInfo = proxy.networkMapSnapshot()
+        return mapOf("peers" to nodeInfo
+                .map { it.legalIdentities.first().name }
+                //filter out myself, notary and eventual network map started by driver
+                .filter { it.organisation !in (SERVICE_NAMES + myLegalName.organisation) })
+    }
+
+    /**
+     * Create DID
+     */
+    @PostMapping(value = "/create",
+            produces = arrayOf(MediaType.APPLICATION_JSON_VALUE))
+    fun createDID() {
+        TODO()
+    }
+}
+
---
+++ did-api/src/main/kotlin/net/corda/did/api/Server.kt
@@ -0,0 +1,24 @@
+package net.corda.did.api
+
+
+import org.springframework.boot.Banner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+/**
+ * A Spring Boot application.
+ */
+
+@SpringBootApplication
+open class Server
+
+/**
+ * Starts our Spring Boot application.
+ */
+fun main(args: Array<String>) {
+    val app = SpringApplication(Server::class.java)
+    app.setBannerMode(Banner.Mode.OFF)
+    app.isWebEnvironment = true
+    app.run(*args)
+}
