4fb4f1e4aecff02d24004e3d2e8c0e9f46b270c7
nitesh_solanki <nitesh_solanki@persistent.com>
2019-05-29 16:06:27 +0530

    Update test case for create did flow

--- did-flows/src/test/kotlin/net/corda/did/flows/CreateDidFlowTests.kt
+++ did-flows/src/test/kotlin/net/corda/did/flows/CreateDidFlowTests.kt
@@ -2,7 +2,9 @@ package net.corda.did.flows
 
 import net.corda.did.state.DidState
 import net.corda.did.state.DidStatus
+import net.corda.did.utils.DIDAlreadyExist
 import org.junit.Test
+import kotlin.test.assertFailsWith
 
 class CreateDidFlowTests : AbstractFlowTestUtils() {
 
@@ -30,5 +32,35 @@ class CreateDidFlowTests : AbstractFlowTestUtils() {
             assert(states.size == 1)
             assert(states[0].state.data.status == DidStatus.VALID)
         }
+    }
+
+    @Test
+    fun `create did should fail if did already exist`() {
+        // crete did
+        val tx = createDID()!!.tx
+        mockNetwork.waitQuiescent()
+
+        // confirm did state with status as 'VALID' on both all 3 nodes
+        w1.transaction {
+            val states = w1.services.vaultService.queryBy(DidState::class.java).states
+            assert(states.size == 1)
+            assert(states[0].state.data.status == DidStatus.VALID)
+        }
+
+        w2.transaction {
+            val states = w2.services.vaultService.queryBy(DidState::class.java).states
+            assert(states.size == 1)
+            assert(states[0].state.data.status == DidStatus.VALID)
+        }
+
+        originator.transaction {
+            val states = originator.services.vaultService.queryBy(DidState::class.java).states
+            assert(states.size == 1)
+            assert(states[0].state.data.status == DidStatus.VALID)
+        }
+
+
+       // mockNetwork.waitQuiescent()
+        assertFailsWith<DIDAlreadyExist> { createDID() }
     }
 }
